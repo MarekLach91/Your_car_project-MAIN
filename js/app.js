b2a6aa8c2917f8e87b3a388cbb0249ada698ef51
Marek Lach <marek.lach91@gmail.com>
2017-04-14 13:10:45 +0200

    all visual

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-$(function(){
+$(function () {
+    
     
     console.log('DOM!');
-    
-var list = document.getElementsByClassName('list')[0],
+      var list = document.getElementsByClassName('list')[0],
         mobile = window.matchMedia("(min-width: 400px)");
  
     mobile.addListener(function (mobile) {
@@ -14,38 +14,29 @@ var list = document.getElementsByClassName('list')[0],
     });
  
     window.addEventListener('DOMContentLoaded', function () {
-        document.getElementById('show-hide-menu').addEventListener('click', function () {
+        document.getElementById('icon-menu').addEventListener('click', function () {
             list.classList.toggle('show');
         });
     });
-  
-  var $films = $('.sec-two');
-    var $open = $films.find('.expand');
-    var $close = $films.find('.hide-a')
-    $open.on('click', function(event){
+    
+   
+    
+    
+    
+    var $menu = $('.sec-two');
+    var $open = $menu.find('.expand');
+    var $close = $menu.find('.hide-a')
+    $open.on('click', function (event) {
         event.preventDefault();
-        
         var $this = $(this);
         var $cont = $this.parent().next();
         $cont.slideDown();
-        
-        
         console.log($cont);
     });
-    
-    var $close = $films.find('.close');
-    $close.on('click', function(event){
+    var $close = $menu.find('.close');
+    $close.on('click', function (event) {
         event.preventDefault();
         var $this = $(this);
         $this.parent().slideUp();
     });
-          
-    
-    
-    
-    
-    
-    
 });
-
-
