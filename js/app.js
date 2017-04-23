0752fc867035082952b574cf7c283f59b63bb3ba
Marek Lach <marek.lach91@gmail.com>
2017-04-23 19:10:10 +0200

    commit 23.04

--- js/app.js
+++ js/app.js
@@ -1,42 +1,39 @@
-$(function () {
+document.addEventListener("DOMContentLoaded", function(){
     
+    console.log('dom');
     
-    console.log('DOM!');
-      var list = document.getElementsByClassName('list')[0],
-        mobile = window.matchMedia("(min-width: 400px)");
- 
-    mobile.addListener(function (mobile) {
-        if (mobile.matches) {
-            list.classList.add('show');
-        } else {
-            list.classList.remove('show');
-        }
-    });
- 
-    window.addEventListener('DOMContentLoaded', function () {
-        document.getElementById('icon-menu').addEventListener('click', function () {
-            list.classList.toggle('show');
-        });
-    });
+   // hamburger menu
     
-   
+    var menu = document.getElementsByClassName('show-hide-menu');
+    console.log(menu);
     
     
     
-    var $menu = $('.sec-two');
-    var $open = $menu.find('.expand');
-    var $close = $menu.find('.hide-a')
-    $open.on('click', function (event) {
-        event.preventDefault();
-        var $this = $(this);
-        var $cont = $this.parent().next();
-        $cont.slideDown();
-        console.log($cont);
-    });
-    var $close = $menu.find('.close');
-    $close.on('click', function (event) {
-        event.preventDefault();
-        var $this = $(this);
-        $this.parent().slideUp();
-    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  // hiding description
+  var carsphoto = document.getElementsByClassName('cars')[0];
+
+
+  function hide(){
+    var elementToHide = this.getElementsByClassName('carmodel')[0];
+    elementToHide.style.display = "none";
+  }
+
+  function show(){
+    var elementToHide = this.getElementsByClassName('carmodel')[0];
+    elementToHide.style.display = "block";
+  }
+
+  carsphoto.addEventListener("mouseover", hide);
+  carsphoto.addEventListener("mouseout", show);
+    
 });
